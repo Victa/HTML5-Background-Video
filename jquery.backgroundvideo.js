a8a9355870c459ca66c4978397b07a1ad2e686af
Desmond Morris <hi@desmondmorris.com>
2013-11-17 17:19:16 -0500

    Adds poster attribute support

--- jquery.backgroundvideo.js
+++ jquery.backgroundvideo.js
@@ -5,37 +5,35 @@
 * Released under the MIT, BSD, and GPL Licenses.
 * based on jQuery Plugin Boilerplate 1.3
 */
-/*
-* jQuery Background video plugin for jQuery
-* ---
-* Copyright 2011, Victor Coulon (http://victorcoulon.fr)
-* Released under the MIT, BSD, and GPL Licenses.
-* based on jQuery Plugin Boilerplate 1.3
-*/
 
 (function($) {
 
   $.backgroundVideo = function(el, options) {
-    
+
     var defaults = {
       videoid: "video_background"
     }
-    
+
     var plugin = this;
-    
+
     plugin.settings = {}
-    
+
     var init = function() {
       plugin.settings = $.extend({}, defaults, options);
       plugin.el = el;
-      
+
       buildVideo();
     }
-    
+
     var buildVideo = function () {
       var html = '';
       html += '<video id="'+plugin.settings.videoid+'" preload="auto" autoplay="autoplay" loop="loop"';
-      html += 'style="display:none;position:fixed;top:0;left:0;bottom:0;right:0;z-index:-100;width:100%;height:100%;">';                                                              
+
+      if (plugin.settings.poster) {
+        html += ' poster="' + plugin.settings.poster + '" ';
+      }
+
+      html += 'style="display:none;position:fixed;top:0;left:0;bottom:0;right:0;z-index:-100;width:100%;height:100%;">';
       for(var i=0; i < plugin.settings.types.length; i++) {
         html += '<source src="'+plugin.settings.path+plugin.settings.filename+'.'+plugin.settings.types[i]+'" type="video/'+plugin.settings.types[i]+'" />';
       }
@@ -46,31 +44,31 @@
       plugin.$videoEl.fadeIn(2000);
       setProportion();
     }
-    
+
     var setProportion = function () {
       var proportion = getProportion();
       plugin.$videoEl.width(proportion*plugin.settings.width);
       plugin.$videoEl.height(proportion*plugin.settings.height);
-      
+
       if (typeof plugin.settings.align !== 'undefined') {
         centerVideo();
       }
     }
-    
+
     var getProportion = function () {
       var windowWidth = $(window).width();
       var windowHeight = $(window).height();
       var windowProportion = windowWidth / windowHeight;
       var origProportion = plugin.settings.width / plugin.settings.height;
       var proportion = windowHeight / plugin.settings.height;
-      
+
       if (windowProportion >= origProportion) {
         proportion = windowWidth / plugin.settings.width;
       }
-      
+
       return proportion;
     }
-    
+
     var centerVideo = function() {
       var centerX = (($(window).width() >> 1) - (plugin.$videoEl.width() >> 1)) | 0;
       var centerY = (($(window).height() >> 1) - (plugin.$videoEl.height() >> 1)) | 0;
@@ -90,9 +88,9 @@
         return;
       }
     }
-    
+
     init();
-    
+
     $(window).resize(function() { setProportion(); });
     plugin.$videoEl.bind('ended', function(){ this.play(); });
   }
